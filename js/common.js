8f0613042d38e14296841e677f379b0a8b09bc0a
KostyaM <graffkost@mail.ru>
2022-10-29 18:09:22 +0300

    in processing 3

--- js/common.js
+++ js/common.js
@@ -1,13 +1,4 @@
-// $(window).keyup(function(e){
-// 	var target = $('.quiz__radio-btn input:focus');
-// 	if (e.keyCode == 9 && $(target).length){
-// 		$(target).parent().addClass('focused');
-// 	}
-// });
-//
-// $('.checkbox-btn input').focusout(function(){
-// 	$(this).parent().removeClass('focused');
-// });
+
 
 // Menu Burger
 const iconMenu = document.querySelector(".menu__icon");
